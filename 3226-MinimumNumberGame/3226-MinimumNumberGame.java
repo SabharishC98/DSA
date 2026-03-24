39fddd471886efa4b696081d9a896838ff0f332f
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:21:28 +0530

    Create 3226-MinimumNumberGame

---
+++ 3226-MinimumNumberGame/3226-MinimumNumberGame.java
@@ -0,0 +1,12 @@
+// Last updated: 3/24/2026, 11:21:28 AM
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i+=2){
+            int t=nums[i];
+            nums[i]=nums[i-1];
+            nums[i-1]=t;
+        }
+        return nums;
+    }
+}
