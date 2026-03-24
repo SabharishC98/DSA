a38b0050a93964651d1a956df54957736b80435a
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:21:13 +0530

    Create 3415-CheckIfGridSatisfiesConditions

---
+++ 3415-CheckIfGridSatisfiesConditions/3415-CheckIfGridSatisfiesConditions.java
@@ -0,0 +1,12 @@
+// Last updated: 3/24/2026, 11:21:12 AM
+class Solution {
+    public boolean satisfiesConditions(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i<grid.length-1 && grid[i][j]!=grid[i+1][j]) return false;
+                if(j<grid[0].length-1 && grid[i][j]==grid[i][j+1]) return false;
+            }
+        }
+        return true;
+    }
+}
