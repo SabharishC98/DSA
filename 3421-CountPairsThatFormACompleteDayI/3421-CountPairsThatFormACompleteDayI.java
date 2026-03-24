f806f6fb84240b217c8c24ceebeeec17c4d1d499
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:21:12 +0530

    Create 3421-CountPairsThatFormACompleteDayI

---
+++ 3421-CountPairsThatFormACompleteDayI/3421-CountPairsThatFormACompleteDayI.java
@@ -0,0 +1,12 @@
+// Last updated: 3/24/2026, 11:21:11 AM
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+        int c=0;
+        for(int i=0;i<hours.length;i++){
+            for(int j=i+1;j<hours.length;j++){
+                if((hours[i]+hours[j])%24==0) c++;
+            }
+        }
+        return c;
+    }
+}
