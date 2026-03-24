f1dc9d8593f9c19735db63d0b1a9a28dd3c4954f
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:32:01 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,28 @@
+// Last updated: 3/24/2026, 11:32:01 AM
+class Solution {
+    public int trap(int[] height) {
+        int l=0,r=height.length-1;
+        int lm=0,rm=0,ans=0;
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>lm){
+                    lm=height[l];
+                }
+                else{
+                    ans+=(lm-height[l]);
+                }
+                l++;
+            }
+            else{
+                if(height[r]>rm){
+                    rm=height[r];
+                }
+                else{
+                    ans+=rm-height[r];
+                }
+                r--;
+            }
+        }
+        return ans;
+    }
+}
