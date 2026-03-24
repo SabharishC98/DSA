e3142b0f33d2077faa8cef389b0cf61f799c5fca
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:24:54 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.java
@@ -0,0 +1,9 @@
+// Last updated: 3/24/2026, 11:24:53 AM
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0) return true;
+        }
+        return false;
+    }
+}
