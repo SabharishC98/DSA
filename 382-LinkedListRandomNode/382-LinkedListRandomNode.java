09495a1e6cd117244460c812809c14eef917ad21
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:28:19 +0530

    Create 382-LinkedListRandomNode

---
+++ 382-LinkedListRandomNode/382-LinkedListRandomNode.java
@@ -0,0 +1,17 @@
+// Last updated: 3/24/2026, 11:28:19 AM
+import java.util.Random;
+
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public Solution(ListNode head) {
+        while(head!=null){
+            list.add(head.val);
+            head=head.next;
+        }
+    }
+    
+    public int getRandom() {
+        double rand=Math.random()*list.size();
+        return list.get((int)rand);
+    }
+}
