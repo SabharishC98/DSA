2e9362f706ac9821bcc43096e9195939eff211ac
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:25:57 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.java
@@ -0,0 +1,18 @@
+// Last updated: 3/24/2026, 11:25:57 AM
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0,j=people.length-1,c=0;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+            }
+            else{
+                j--;
+            }
+            c++;
+        }
+        return c;
+    }
+}
