8cc342269e4680885b46e890ef184c0a48b1196f
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:25:41 +0530

    Create 981-DeleteColumnsToMakeSorted

---
+++ 981-DeleteColumnsToMakeSorted/981-DeleteColumnsToMakeSorted.java
@@ -0,0 +1,15 @@
+// Last updated: 3/24/2026, 11:25:41 AM
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int c=0;
+        for(int i=0;i<strs[0].length();i++){
+            for(int j=1;j<strs.length;j++){
+                if(strs[j].charAt(i)<strs[j-1].charAt(i)){
+                    c++;
+                    break;
+                }
+            }
+        }
+        return c;
+    }
+}
