03e7310748ca7e7d07b9375f8ef0621213734618
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:25:16 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.py
@@ -0,0 +1,8 @@
+# Last updated: 3/24/2026, 11:25:16 AM
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n%2==0 
