6ae6c573814ccfed014290ff21c8ec3273e1ffa6
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:21:55 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 3/24/2026, 11:21:54 AM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
