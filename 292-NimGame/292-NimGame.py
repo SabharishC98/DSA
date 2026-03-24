bf448d938784c3c4475de9968a789741eca94e20
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:28:46 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.py
@@ -0,0 +1,8 @@
+# Last updated: 3/24/2026, 11:28:45 AM
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n%4!=0
