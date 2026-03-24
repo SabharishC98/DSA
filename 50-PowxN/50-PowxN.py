447596435528ce4dbf4100c12799086894ce5bbc
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:31:51 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,9 @@
+# Last updated: 3/24/2026, 11:31:51 AM
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return pow(x,n)
