bd2e947052d174c0ed814f7482d6884e09f8168a
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:28:59 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,21 @@
+// Last updated: 3/24/2026, 11:28:58 AM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int []arr=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-97]+=1;
+        }
+        for(int i=0;i<t.length();i++){
+            arr[t.charAt(i)-97]-=1;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
