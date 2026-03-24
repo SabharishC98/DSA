4cc0a6588a0e7e3a14c1324c2f6235f755510aef
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:32:00 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.java
@@ -0,0 +1,27 @@
+// Last updated: 3/24/2026, 11:32:00 AM
+class Solution {
+    public String multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + pos[p2];
+
+                pos[p2] = sum % 10;
+                pos[p1] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int digit : pos) {
+            if (!(sb.length() == 0 && digit == 0)) { // skip leading zeros
+                sb.append(digit);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
