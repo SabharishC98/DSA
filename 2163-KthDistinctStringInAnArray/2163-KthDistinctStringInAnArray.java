bafa307f7e15899195bb9dae0b08ca8307b53ded
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:23:10 +0530

    Create 2163-KthDistinctStringInAnArray

---
+++ 2163-KthDistinctStringInAnArray/2163-KthDistinctStringInAnArray.java
@@ -0,0 +1,15 @@
+// Last updated: 3/24/2026, 11:23:09 AM
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(map.get(arr[i])==1){
+                if(--k==0) return arr[i];
+            }
+        }
+        return "";
+    }
+}
