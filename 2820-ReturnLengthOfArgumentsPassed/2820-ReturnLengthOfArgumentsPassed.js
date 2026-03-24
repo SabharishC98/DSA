2965ad17cc73c2a25d9e9311e8d48dc847f680bd
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:21:44 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 3/24/2026, 11:21:44 AM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
