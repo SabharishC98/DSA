eca6041ec88d6e7e7c39e83b025e10e754b66a1f
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:26:32 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,12 @@
+// Last updated: 3/24/2026, 11:26:32 AM
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+        // String ans="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)>=65 && s.charAt(i)<=90) ans+=(char)(s.charAt(i)+32);
+        //     else ans+=s.charAt(i);
+        // }
+        // return ans;
+    }
+}
