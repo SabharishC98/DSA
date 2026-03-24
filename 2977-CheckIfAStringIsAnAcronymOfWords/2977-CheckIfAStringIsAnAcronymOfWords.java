ef6d399cfaa9636e9721b5a6e8be5a5fde25a9b6
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-03-24 11:21:34 +0530

    Create 2977-CheckIfAStringIsAnAcronymOfWords

---
+++ 2977-CheckIfAStringIsAnAcronymOfWords/2977-CheckIfAStringIsAnAcronymOfWords.java
@@ -0,0 +1,13 @@
+// Last updated: 3/24/2026, 11:21:33 AM
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        if(words.size()!=s.length()){
+            return false;
+        }
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<words.size();i++){
+            ans.append(words.get(i).charAt(0));
+        }
+        return s.equals(ans.toString());
+    }
+}
