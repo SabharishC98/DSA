a5403de87c551ed70f7d3c52681dcac04319f492
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-05-26 23:29:58 +0530

    File updated at 5/26/2026, 11:29:57 PM

--- 1963-MinimumNumberofSwapstoMaketheStringBalanced/1963-MinimumNumberofSwapstoMaketheStringBalanced.java
+++ 1963-MinimumNumberofSwapstoMaketheStringBalanced/1963-MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -1,44 +1,26 @@
-// Last updated: 5/26/2026, 11:18:57 PM
+// Last updated: 5/26/2026, 11:29:57 PM
 1class Solution {
-2    public int minSwaps(String s) {
-3        // int open = 0, ans = 0;
-4        
-5        // // Pass 1: count unmatched ']'
-6        // int unmatched = 0;
-7        // for (char c : s.toCharArray()) {
-8        //     if (c == '[') open++;
-9        //     else {
-10        //         if (open > 0) open--;  // matched
-11        //         else unmatched++;       // unmatched ']'
-12        //     }
-13        // }
-14        
-15        // // Each swap fixes 2 unmatched brackets
-16        // // unmatched is always even (valid input guaranteed)
-17        // return (unmatched + 1) / 2;
-18
-19
-20        int ans=0,c=0,o=0;
-21        int n=s.length()-1,i=0;
-22        while(i<n){
-23            if(s.charAt(i)==']'){
-24                c++;
-25            }
-26            else{
-27                o++;
-28            }
-29            if(c>o){
-30                while(i<n && s.charAt(n)==']'){
-31                    n--;
-32                }
-33                n--;
-34                ans++;
-35                c--;
-36                o++;
-37            }
-38
-39            i++;
-40        }
-41        return ans;
-42    }
-43}
+2    public String minRemoveToMakeValid(String s) {
+3        Stack<Integer> stack = new Stack<>();
+4        Set<Integer> remove = new HashSet<>();
+5        
+6        for (int i = 0; i < s.length(); i++) {
+7            char c = s.charAt(i);
+8            if (c == '(') {
+9                stack.push(i);
+10            } else if (c == ')') {
+11                if (!stack.isEmpty()) stack.pop();  // matched
+12                else remove.add(i);                  // unmatched ')'
+13            }
+14        }
+15        
+16        while (!stack.isEmpty()) remove.add(stack.pop());
+17        
+18        StringBuilder sb = new StringBuilder();
+19        for (int i = 0; i < s.length(); i++) {
+20            if (!remove.contains(i)) sb.append(s.charAt(i));
+21        }
+22        
+23        return sb.toString();
+24    }
+25}
