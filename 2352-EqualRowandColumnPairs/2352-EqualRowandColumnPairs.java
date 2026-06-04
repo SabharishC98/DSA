44e2cdf52e4497887f44d0301f5bb1486c8030a5
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-06-04 11:55:00 +0530

    Create 2352-EqualRowandColumnPairs

---
+++ 2352-EqualRowandColumnPairs/2352-EqualRowandColumnPairs.java
@@ -0,0 +1,29 @@
+// Last updated: 6/4/2026, 11:54:56 AM
+1class Solution {
+2    public int equalPairs(int[][] grid) {
+3        List<String> colset=new ArrayList<>();
+4        List<String> rowset=new ArrayList<>();
+5        int n=grid.length;
+6        for(int i=0;i<n;i++){
+7            String row="";
+8            for(int j=0;j<n;j++){
+9                row+=String.valueOf(grid[i][j]+" ");
+10            }
+11            rowset.add(row);
+12        }
+13        for(int i=0;i<n;i++){
+14            String col="";
+15            for(int j=0;j<n;j++){
+16                col+=String.valueOf(grid[j][i]+" ");
+17            }
+18            colset.add(col);
+19        }
+20        int c=0;
+21        for(int i=0;i<n;i++){
+22            for(int j=0;j<n;j++){
+23                if(rowset.get(i).equals(colset.get(j))) c++;
+24            }
+25        }
+26        return c;
+27    }
+28}
