0c369defb8a3d734978ff1b92a18328f4f73d7d4
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-06-16 10:23:54 +0530

    File updated at 6/16/2026, 10:23:54 AM

--- 1545-FindKthBitinNthBinaryString/1545-FindKthBitinNthBinaryString.java
+++ 1545-FindKthBitinNthBinaryString/1545-FindKthBitinNthBinaryString.java
@@ -1,17 +1,15 @@
-// Last updated: 6/16/2026, 10:19:25 AM
+// Last updated: 6/16/2026, 10:23:54 AM
 1class Solution {
 2    public char findKthBit(int n, int k) {
 3        StringBuilder sb=new StringBuilder("0");
 4        for(int i=1;i<n;i++){
-5            StringBuilder temp=new StringBuilder("");
-6            for(int j=0;j<sb.length();j++){
-7                temp.append(sb.charAt(j)=='0'?'1':'0');
-8            }
-9            temp.reverse();
-10            sb.append('1');
-11            sb.append(temp);
-12        }
-13        // System.out.println(sb);
-14        return sb.charAt(k-1);
-15    }
-16}
+5            String flipped = sb.toString()
+6                .replace('0', '2')
+7                .replace('1', '0')
+8                .replace('2', '1');
+9            sb.append('1').append(new StringBuilder(flipped).reverse());
+10        }
+11        // System.out.println(sb);
+12        return sb.charAt(k-1);
+13    }
+14}
