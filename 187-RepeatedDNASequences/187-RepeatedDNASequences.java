c7a38f64760cd5ccf714aa6457b0a439c2af5b43
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-06-16 06:33:52 +0530

    File updated at 6/16/2026, 6:33:47 AM

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.java
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.java
@@ -1,23 +1,22 @@
-// Last updated: 6/16/2026, 5:21:28 AM
+// Last updated: 6/16/2026, 6:33:47 AM
 1class Solution {
-2    public List<String> findRepeatedDnaSequences(String s) {
-3        HashMap<String,Integer> map=new HashMap<>();
-4        List<String> ans=new ArrayList<>();
-5        if(s.length()<=10) return ans;
-6        StringBuilder sb=new StringBuilder();
-7        for(int i=0;i<10;i++){
-8            sb.append(s.charAt(i));
-9        }
-10        map.put(sb.toString(),map.getOrDefault(sb.toString(), 0)+1);
-11        for(int i=10;i<s.length();i++){
-12            sb.deleteCharAt(0);
-13            sb.append(s.charAt(i));
-14            map.put(sb.toString(),map.getOrDefault(sb.toString(), 0)+1);
-15        }
-16        // System.out.println(map);
-17        for(String i:map.keySet()){
-18            if(map.get(i)>1) ans.add(i);
-19        }
-20        return ans;
-21    }
-22}
+2    public int maximumSwap(int num) {
+3        char[] digits = String.valueOf(num).toCharArray();
+4        HashMap<Integer, Integer> map = new HashMap<>();
+5        for(int i=0;i<digits.length;i++){
+6            map.put(digits[i]-'0',i);  
+7        }
+8        for(int i=0;i<digits.length;i++){
+9            for(int j=9;j>digits[i]-'0';j--){
+10                if(map.containsKey(j) && map.get(j)>i){
+11                    int swapIdx=map.get(j);
+12                    char t=digits[i];
+13                    digits[i]=digits[swapIdx];
+14                    digits[swapIdx]=t;
+15                    return Integer.parseInt(new String(digits));
+16                }
+17            }
+18        }
+19        return num;
+20    }
+21}
