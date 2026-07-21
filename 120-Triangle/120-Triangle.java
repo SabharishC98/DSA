6fdd2f649ff12740b2da127afb319b4824fc6a24
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-07-21 09:39:41 +0530

    File updated at 7/21/2026, 9:39:38 AM

--- 120-Triangle/120-Triangle.java
+++ 120-Triangle/120-Triangle.java
@@ -1,28 +1,30 @@
-// Last updated: 3/24/2026, 11:30:34 AM
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        List<List<Integer>> dp=new ArrayList<>();
-        for(int i=0;i<triangle.size();i++){
-            dp.add(new ArrayList<>());
-            for(int j=0;j<triangle.get(i).size();j++){
-                if(i==0 && j==0) dp.get(i).add(triangle.get(i).get(j));
-                else if(j>0 && j<dp.get(i-1).size()){
-                    dp.get(i).add(Math.min(dp.get(i-1).get(j),dp.get(i-1).get(j-1))+triangle.get(i).get(j));
-                }
-                else if(j==0){
-                    dp.get(i).add(dp.get(i-1).get(j)+triangle.get(i).get(j));
-                }
-                else{
-                    dp.get(i).add(dp.get(i-1).get(j-1)+triangle.get(i).get(j));
-                }
-            }
-        }
-
-        int min=Integer.MAX_VALUE;
-        int n=triangle.get(triangle.size()-1).size();
-        for(int i=0;i<n;i++){
-            min=Math.min(min,dp.get(triangle.size()-1).get(i));
-        }
-        return min;
-    }
-}
+// Last updated: 7/21/2026, 9:39:38 AM
+1class Solution {
+2    public int minimumTotal(List<List<Integer>> triangle) {
+3        List<List<Integer>> dp=new ArrayList<>();
+4        for(int i=0;i<triangle.size();i++){
+5            dp.add(new ArrayList<>());
+6            for(int j=0;j<triangle.get(i).size();j++){
+7                if(i==0 && j==0){
+8                    dp.get(i).add(triangle.get(i).get(j));
+9                }
+10                else if(j==triangle.get(i).size()-1){
+11                    dp.get(i).add(dp.get(i-1).get(dp.get(i-1).size()-1)+triangle.get(i).get(j));
+12                }
+13                else if(j==0){
+14                    dp.get(i).add(dp.get(i-1).get(0)+triangle.get(i).get(j));
+15                }
+16                else{
+17                    dp.get(i).add(Math.min(dp.get(i-1).get(j-1),dp.get(i-1).get(j))+triangle.get(i).get(j));
+18                }
+19            }
+20        }
+21        // for(int i=0;i<dp.size();i++) System.out.println(dp.get(i));
+22        int min=Integer.MAX_VALUE;
+23        // System.out.println(triangle.get(triangle.size()-1));
+24        for(int i=0;i<dp.get(dp.size()-1).size();i++){
+25            min=Math.min(min,dp.get(dp.size()-1).get(i));
+26        }
+27        return min;
+28    }
+29}
