3553e9e25fce5574367b7a857200943aaa6e2275
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-08-19 01:38:21 +0530

    File updated at 8/19/2026, 1:38:21 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,19 +1,22 @@
-// Last updated: 8/3/2026, 2:22:44 PM
+// Last updated: 8/19/2026, 1:38:21 AM
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
-3        int n=text1.length(),m=text2.length();
-4        int [][]dp=new int[n+1][m+1];
-5        dp[0][0]=0;
-6        for(int i=1;i<=n;i++){
-7            for(int j=1;j<=m;j++){
-8                if(text1.charAt(i-1)==text2.charAt(j-1)){
-9                    dp[i][j]=dp[i-1][j-1]+1;
-10                }
-11                else{
-12                    dp[i][j]=Math.max(dp[i-1][j-1],Math.max(dp[i-1][j],dp[i][j-1]));
-13                }
-14            }
+3        int[][] memo=new int[text1.length()][text2.length()];
+4        for(int[] i:memo){
+5            Arrays.fill(i,-1);
+6        }
+7        return process(text1,text2,0,0,memo);
+8    }
+9    public int process(String s,String p,int i,int j,int[][] memo){
+10        if(i==s.length() || j==p.length()){
+11            return 0;
+12        }
+13        if(memo[i][j]!=-1){
+14            return memo[i][j];
 15        }
-16        return dp[n][m];
-17    }
-18}
+16        if(s.charAt(i)==p.charAt(j)){
+17            return memo[i][j]=1+process(s,p,i+1,j+1,memo);
+18        }
+19        return memo[i][j]=Math.max(process(s,p,i+1,j,memo),process(s,p,i,j+1,memo));
+20    }
+21}
