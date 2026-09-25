9e193dd0d48c4a42c23b3f418290e631f5331578
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:42:55 +0530

    Create 868-BinaryGap

---
+++ 868-BinaryGap/868-BinaryGap.java
@@ -0,0 +1,21 @@
+// Last updated: 9/25/2026, 2:42:55 PM
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1;  
+        int max = 0;
+        int index = 0;  
+
+        while (n > 0) {
+            if ((n & 1) == 1) { 
+                if (last != -1) {
+                    max = Math.max(max, index - last);
+                }
+                last = index;
+            }
+            index++;
+            n >>= 1; 
+        }
+
+        return max;
+    }
+}
