d04b60a4b28db0db058db688bdea44ac1ab8a696
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:29:10 +0530

    Create 2667-CreateHelloWorldFunction

---
+++ 2667-CreateHelloWorldFunction/2667-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 9/25/2026, 2:29:10 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
