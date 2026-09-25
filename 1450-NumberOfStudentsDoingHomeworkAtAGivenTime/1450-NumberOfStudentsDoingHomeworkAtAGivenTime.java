73683542c8ddc44c5064888816ddb954502f5039
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:37:08 +0530

    Create 1450-NumberOfStudentsDoingHomeworkAtAGivenTime

---
+++ 1450-NumberOfStudentsDoingHomeworkAtAGivenTime/1450-NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,10 @@
+// Last updated: 9/25/2026, 2:37:08 PM
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) count++;
+        }
+        return count;
+    }
+}
