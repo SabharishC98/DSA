2ccf60f171c8a32c815ad7882529bd69980ab2ea
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:28:18 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ 2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.java
@@ -0,0 +1,10 @@
+// Last updated: 9/25/2026, 2:28:18 PM
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].indexOf(x)!=-1) ans.add(i);
+        }
+        return ans;
+    }
+}
