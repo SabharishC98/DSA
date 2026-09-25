0b5ee2305038a3d428778077745447d66e666ffd
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:36:42 +0530

    Create 1539-KthMissingPositiveNumber

---
+++ 1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 9/25/2026, 2:36:41 PM
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int prev = 0;
+        for (int num : arr) {
+            int missing = num - prev - 1;
+            if (k <= missing) {
+                return prev + k;
+            }
+            k -= missing;
+            prev = num;
+        }
+        return prev + k;
+    }
+}
