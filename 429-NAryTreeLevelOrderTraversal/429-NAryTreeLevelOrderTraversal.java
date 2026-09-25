1af860053318c9b950fff2a56368075f7feee613
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:44:15 +0530

    Create 429-NAryTreeLevelOrderTraversal

---
+++ 429-NAryTreeLevelOrderTraversal/429-NAryTreeLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+// Last updated: 9/25/2026, 2:44:15 PM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> list=new ArrayList<>();
+        Queue<Node> q=new LinkedList<>();
+        if(root==null) return list;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int len=q.size();
+            List<Integer> temp=new ArrayList<>();
+            for(int i=0;i<len;i++){
+                Node cur=q.poll();
+                for(int j=0;j<cur.children.size();j++){
+                    q.offer(cur.children.get(j));
+                }
+                temp.add(cur.val);
+            }
+            list.add(temp);
+        }
+        return list;
+    }
+}
