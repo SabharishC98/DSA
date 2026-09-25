e815efd87b59d3bce69e4d5a831487a2197723f6
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:33:41 +0530

    Create 2011-FinalValueOfVariableAfterPerformingOperations

---
+++ 2011-FinalValueOfVariableAfterPerformingOperations/2011-FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,11 @@
+// Last updated: 9/25/2026, 2:33:40 PM
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String op : operations) {
+            if (op.charAt(1) == '+') x++;
+            else x--;
+        }
+        return x;
+    }
+}
