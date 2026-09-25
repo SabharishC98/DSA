312b52f760c7551e040182cff97a5a5b592664d7
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:29:06 +0530

    Create 2710-RemoveTrailingZerosFromAString

---
+++ 2710-RemoveTrailingZerosFromAString/2710-RemoveTrailingZerosFromAString.java
@@ -0,0 +1,19 @@
+// Last updated: 9/25/2026, 2:29:05 PM
+class Solution {
+    public String removeTrailingZeros(String num) {
+        StringBuilder s=new StringBuilder("");
+        int flag=0;
+        for(int i=num.length()-1;i>=0;i--){
+            if(flag==0 && num.charAt(i)=='0'){continue; }
+            else{
+                flag=1;
+                s.append(num.charAt(i));
+            }
+        }
+        StringBuilder ans=new StringBuilder("");
+        for(int i=s.length()-1;i>=0;i--){
+            ans.append(s.charAt(i));
+        }
+        return ans.toString();
+    }
+}
