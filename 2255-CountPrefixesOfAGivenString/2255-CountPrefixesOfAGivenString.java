a269410d7f09c487577a1c9f41bdd56dc564454a
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:32:08 +0530

    Create 2255-CountPrefixesOfAGivenString

---
+++ 2255-CountPrefixesOfAGivenString/2255-CountPrefixesOfAGivenString.java
@@ -0,0 +1,20 @@
+// Last updated: 9/25/2026, 2:32:08 PM
+class Solution {
+    public int countPrefixes(String[] words, String s) {
+        int c=0;
+        for(int i=0;i<words.length;i++){
+            if(s.startsWith(words[i])) c++;
+            // if(words[i].equals(s)) c++;
+            // else if(words[i].length()>s.length()) continue;
+            // else{
+            //     int j=0;
+            //     while(j<words[i].length()){
+            //         if(s.charAt(j)!=words[i].charAt(j)) break;
+            //         j++;
+            //     }
+            //     if(j==words[i].length()) c++;
+            // }
+        }
+        return c;
+    }
+}
