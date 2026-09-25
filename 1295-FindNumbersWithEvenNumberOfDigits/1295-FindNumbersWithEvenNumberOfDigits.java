eafcc4872aea1fc2bf1d466cedddce35ec8ec104
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:38:26 +0530

    Create 1295-FindNumbersWithEvenNumberOfDigits

---
+++ 1295-FindNumbersWithEvenNumberOfDigits/1295-FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,15 @@
+// Last updated: 9/25/2026, 2:38:26 PM
+class Solution {
+    public int findNumbers(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            while(nums[i]>0){
+                count++;
+                nums[i]/=10;
+            }
+            if(count%2==0) c++;
+        }
+        return c;
+    }
+}
