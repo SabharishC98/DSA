083ce5c8fd2041e090e5038bf32f3b1ac67a3126
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:43:28 +0530

    Create 705-DesignHashset

---
+++ 705-DesignHashset/705-DesignHashset.java
@@ -0,0 +1,29 @@
+// Last updated: 9/25/2026, 2:43:27 PM
+class MyHashSet {
+    ArrayList<Integer> n=new ArrayList<>();
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        if(!n.contains(key)) n.add(key);
+    }
+    
+    public void remove(int key) {
+        if(n.indexOf(key)!=-1)
+        n.remove(n.indexOf(key));
+            
+    }
+    
+    public boolean contains(int key) {
+        return n.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
