6fe31871bf9e16a41451e921ce986189ef6ee844
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:44:31 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.java
@@ -0,0 +1,12 @@
+// Last updated: 9/25/2026, 2:44:31 PM
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+        // String ans="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)>=65 && s.charAt(i)<=90) ans+=(char)(s.charAt(i)+32);
+        //     else ans+=s.charAt(i);
+        // }
+        // return ans;
+    }
+}
