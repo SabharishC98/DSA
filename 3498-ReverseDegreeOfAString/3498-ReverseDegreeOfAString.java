4c3c8005e166cb9f01d5fe5bc76ed5ede59516fd
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:26:07 +0530

    Create 3498-ReverseDegreeOfAString

---
+++ 3498-ReverseDegreeOfAString/3498-ReverseDegreeOfAString.java
@@ -0,0 +1,10 @@
+// Last updated: 9/25/2026, 2:26:07 PM
+class Solution {
+    public int reverseDegree(String s) {
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            sum+=(26-s.charAt(i)+97)*(i+1);
+        }
+        return sum;
+    }
+}
