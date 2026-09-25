75be88810c22827b888ade3b5428fed10e07fb2c
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:47:45 +0530

    Create 433-MinimumGeneticMutation

---
+++ 433-MinimumGeneticMutation/433-MinimumGeneticMutation.java
@@ -0,0 +1,56 @@
+// Last updated: 9/25/2026, 2:47:45 PM
+class Solution {
+    public int minMutation(String beginWord, String endWord, String[] words) {
+        List<String> wordList=new ArrayList<>();
+        for(String i:words){
+            wordList.add(i);
+        }
+        if (!wordList.contains(endWord)) {
+            return -1;
+        }
+
+        int L = beginWord.length();
+
+        // Map: generic pattern -> list of words
+        Map<String, List<String>> patternMap = new HashMap<>();
+
+        for (String word : wordList) {
+            for (int i = 0; i < L; i++) {
+                String pattern = word.substring(0, i) + "*" + word.substring(i + 1);
+                patternMap.computeIfAbsent(pattern, k -> new ArrayList<>()).add(word);
+            }
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+
+        Map<String, Integer> distance = new HashMap<>();
+        distance.put(beginWord, 1);
+
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            int level = distance.get(current);
+
+            for (int i = 0; i < L; i++) {
+                String pattern = current.substring(0, i) + "*" + current.substring(i + 1);
+
+                List<String> neighbors = patternMap.getOrDefault(pattern, Collections.emptyList());
+                for (String next : neighbors) {
+                    if (next.equals(endWord)) {
+                        return level ;
+                    }
+
+                    if (!distance.containsKey(next)) {
+                        distance.put(next, level + 1);
+                        queue.offer(next);
+                    }
+                }
+
+                // Important optimization
+                patternMap.put(pattern, Collections.emptyList());
+            }
+        }
+
+        return -1;
+    }
+}
