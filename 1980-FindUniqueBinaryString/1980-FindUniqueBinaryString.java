8d03771d81ab65ce6ac912bf86f5e46c915dbdbd
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:34:04 +0530

    Create 1980-FindUniqueBinaryString

---
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.java
@@ -0,0 +1,12 @@
+// Last updated: 9/25/2026, 2:34:04 PM
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder s=new StringBuilder();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i].charAt(i)=='0')
+            s.append('1');
+            else s.append('0');
+        }
+        return s.toString();
+    }
+}
