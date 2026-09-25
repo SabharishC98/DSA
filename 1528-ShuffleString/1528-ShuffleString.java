af2146c5bd6a25f89a397d685d096f4d0e67dfdf
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:36:38 +0530

    Create 1528-ShuffleString

---
+++ 1528-ShuffleString/1528-ShuffleString.java
@@ -0,0 +1,10 @@
+// Last updated: 9/25/2026, 2:36:38 PM
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+        return new String(result);
+    }
+}
