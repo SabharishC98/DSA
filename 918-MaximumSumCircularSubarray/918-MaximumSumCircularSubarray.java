64cf728462dae7146a35a5a86a8aa354ea9d829f
SabharishC98 <167817120+SabharishC98@users.noreply.github.com>
2026-09-25 14:42:05 +0530

    Create 918-MaximumSumCircularSubarray

---
+++ 918-MaximumSumCircularSubarray/918-MaximumSumCircularSubarray.java
@@ -0,0 +1,17 @@
+// Last updated: 9/25/2026, 2:42:05 PM
+class Solution {
+    public int maxSubarraySumCircular(int[] arr) {
+        int sum=arr[0];
+        int cur=arr[0],cmin=arr[0];
+        int max=arr[0],min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            sum+=arr[i];
+            cur=Math.max(cur+arr[i],arr[i]);
+            max=Math.max(max,cur);
+            cmin=Math.min(cmin+arr[i],arr[i]);
+            min=Math.min(min,cmin);
+        }
+        if(max<0) return max;
+        return Math.max(max,sum-min);
+    }
+}
